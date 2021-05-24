572d36c660a3cf03271ddf34df5eeae89723673e
ZRCrawford15 <48399752+ZRCrawford15@users.noreply.github.com>
2021-05-23 18:17:53 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -7,11 +7,13 @@ var bodyParser = require('body-parser');
 
 var request = require('request');
 
+app.use(express.static('public'));
 let key = 'd38e842ac476454d18109f49c5cef3e6';
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(session({secret:'SuperSecretPassword'}));
+
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
@@ -33,6 +35,20 @@ app.get('/',function(req,res,next){
   res.render('toDo',context);
 });
 
+app.get('/', function(req, res, next))
+  let weather = {};
+  request('http://api.openweathermap.org/data/2.5/weather?q=' + req.body.city + '&APPID=' + key, function(err, response, body){
+    if (!err && response.statusCode < 400) {
+      weather.owm = body;
+    } else {
+      console.log(err);
+      if(response) {
+        console.log(response.statusCode)
+      }
+    }
+    next(err)
+  })
+
 app.post('/',function(req,res){
   var context = {};
 
@@ -50,18 +66,18 @@ app.post('/',function(req,res){
 
   if(req.body['Add Item']){
     req.session.toDo.push({"name":req.body.name, "city":req.body.city, "id":req.session.curId});
-    let context = {}
-    request('http://api.openweathermap.org/data/2.5/weather?q=' + req.body.city + '&APPID=' + key, function(err, response, body) {
-      if (!err && response.statusCode < 400) {
-        context.owm = body;
-      } else {
-        console.log(err);
-        if (response) {
-          console.log(response.statusCode);
-        }
-      }
-      next(err);
-    })
+    // let context = {}
+    // request('http://api.openweathermap.org/data/2.5/weather?q=' + req.body.city + '&APPID=' + key, function(err, response, body) {
+    //   if (!err && response.statusCode < 400) {
+    //     context.owm = body;
+    //   } else {
+    //     console.log(err);
+    //     if (response) {
+    //       console.log(response.statusCode);
+    //     }
+    //   }
+    //   next(err);
+    // })
     req.session.curId++;
   }
 
